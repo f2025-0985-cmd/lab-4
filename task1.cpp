c8d8d08d3d74719154c17c67c69da6aa3aa56d94
f2025-0985-cmd <f2025-0985@bnu.edu.pk>
2025-10-24 14:47:02 +0500

    Add task1.cpp to display user information

---
+++ task1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "\tName       :\tAli haider\n";
+    cout << "\tDepartment :\tSCIT\n";
+    cout << "\tQuote      :\t\"Knowledge is power.\"\n";
+    return 0;
+}
