021c57b441f0aadc7e29de75e4ee2c55b581320b
f2025-0985-cmd <f2025-0985@bnu.edu.pk>
2025-10-24 14:58:26 +0500

    Create task2.cpp

---
+++ task2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string month1, month2, month3;
+    double rain1, rain2, rain3, average;
+
+    cout << "Enter the name of the first month: ";
+    cin >> month1;
+    cout << "Enter the rainfall (in inches) for " << month1 << ": ";
+    cin >> rain1;
+
+    cout << "\nEnter the name of the second month: ";
+    cin >> month2;
+    cout << "Enter the rainfall (in inches) for " << month2 << ": ";
+    cin >> rain2;
+
+    cout << "\nEnter the name of the third month: ";
+    cin >> month3;
+    cout << "Enter the rainfall (in inches) for " << month3 << ": ";
+    cin >> rain3;
+
+    average = (rain1 + rain2 + rain3) / 3.0;
+
+
+    cout << "\nThe average rainfall for " << month1 << ", " << month2 << ", and "
+         << month3 << " is " << average << " inches.\n";
+
+
+    return 0;
+}
